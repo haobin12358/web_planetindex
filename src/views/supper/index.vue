2c270b6e197d051a8f371d01b2e633b78205c9ea
maoyaY <17774012962@163.com>
2019-04-02 17:32:00 +0800

    上传图片

--- src/views/supper/index.vue
+++ src/views/supper/index.vue
@@ -4,23 +4,23 @@
       <el-form ref="form" :model="form" label-width="120px" :rules="ruleForm" label-position="left">
         <div v-if="!secondStep">
           <h3 class="m-title">资料填写</h3>
-          <el-form-item label="手机号">
-            <el-input v-model="form.name"></el-input>
+          <el-form-item label="手机号" prop="suloginphone">
+            <el-input v-model="form.suloginphone"></el-input>
           </el-form-item>
-          <el-form-item label="供应商名称">
-            <el-input v-model="form.name"></el-input>
+          <el-form-item label="供应商名称" prop="suname">
+            <el-input v-model="form.suname"></el-input>
           </el-form-item>
-          <el-form-item label="供应商联系人">
-            <el-input v-model="form.name"></el-input>
+          <el-form-item label="供应商联系人" prop="sulinkman">
+            <el-input v-model="form.sulinkman"></el-input>
           </el-form-item>
-          <el-form-item label="供应商联系电话">
-            <el-input v-model="form.name"></el-input>
+          <el-form-item label="供应商联系电话" prop="sulinkphone">
+            <el-input v-model="form.sulinkphone"></el-input>
           </el-form-item>
-          <el-form-item label="供应商邮箱">
-            <el-input v-model="form.name"></el-input>
+          <el-form-item label="供应商邮箱" prop="suemail">
+            <el-input v-model="form.suemail"></el-input>
           </el-form-item>
-          <el-form-item label="供应商地址">
-            <el-input v-model="form.name"></el-input>
+          <el-form-item label="供应商地址" prop="suaddress">
+            <el-input v-model="form.suaddress"></el-input>
           </el-form-item>
           <div   class="m-text-center" >
             <span class="m-supper-btn" @click="secondStep = true">下一步</span>
@@ -28,21 +28,24 @@
 
         </div>
         <div v-else>
-          <el-form-item label="银行卡号">
-            <el-input v-model="form.name"></el-input>
+          <el-form-item label="银行卡号" prop="subanksn">
+            <el-input v-model="form.subanksn"></el-input>
           </el-form-item>
-          <el-form-item label="银行卡开户行">
-            <el-input v-model="form.name"></el-input>
+          <el-form-item label="银行卡开户行" prop="subankname">
+            <el-input v-model="form.subankname"></el-input>
           </el-form-item>
-          <el-form-item label="合同">
+          <el-form-item label="合同" prop="sucontract">
             <el-upload
-              :action="uploadUrl"
+              :action="uploadVoucherUrl"
+              :file-list="suContractUrl"
               list-type="picture-card"
-              :show-file-list="false"
-              :on-success="handleHeaderSuccess"
+              accept="image/*"
               :before-upload="beforePicUpload"
               :on-preview="handlePictureCardPreview"
-              :on-remove="handleRemove">
+              :on-remove="handleConstractRemove"
+              :http-request="uploadContract"
+              :limit="20"
+              :multiple="true">
               <i class="el-icon-plus"></i>
             </el-upload>
             <span class="m-alert">请上传合同图片</span>
@@ -50,37 +53,60 @@
               <img width="100%" :src="dialogImageUrl" alt="">
             </el-dialog>
           </el-form-item>
-          <el-form-item label="营业执照">
+          <el-form-item label="营业执照" prop="subusinesslicense">
             <el-upload
               :action="uploadUrl"
+              class="avatar-uploader"
               list-type="picture-card"
+              accept="image/*"
+              :on-success="handleHeaderSuccess"
               :on-preview="handlePictureCardPreview"
-              :on-remove="handleRemove">
+              :on-remove="handleLicenseRemove">
+              <img v-if="form.subusinesslicense" :key="form.subusinesslicense" v-lazy="form.subusinesslicense"
+                   class="avatar">
               <i class="el-icon-plus"></i>
             </el-upload>
             <span class="m-alert">请上传营业执照图片</span>
           </el-form-item>
-          <el-form-item label="注册资金">
-            <el-input v-model="form.name"></el-input>
+          <el-form-item label="注册资金" prop="suregisteredfund">
+            <el-input v-model="form.suregisteredfund"></el-input>
           </el-form-item>
-          <el-form-item label="主营类目">
-            <el-input v-model="form.name"></el-input>
+          <el-form-item label="主营类目" prop="sumaincategory">
+            <el-input v-model="form.sumaincategory"></el-input>
           </el-form-item>
-          <el-form-item label="注册时间">
-            <el-input v-model="form.name"></el-input>
+          <el-form-item label="注册时间" prop="suregisteredtime">
+            <el-input v-model="form.suregisteredtime"></el-input>
           </el-form-item>
-          <el-form-item label="法人姓名">
-            <el-input v-model="form.name"></el-input>
+          <el-form-item label="法人姓名" prop="sulegalperson">
+            <el-input v-model="form.sulegalperson"></el-input>
           </el-form-item>
-          <el-form-item label="法人身份证">
+          <el-form-item label="法人身份证" required>
             <el-upload
               :action="uploadUrl"
+              class="avatar-uploader"
               list-type="picture-card"
+              accept="image/*"
+              :on-success="handleFrontSuccess"
               :on-preview="handlePictureCardPreview"
-              :on-remove="handleRemove">
+              :on-remove="handleFrontRemove">
+              <img v-if="form.sulegalpersonidcardfront" :key="form.sulegalpersonidcardfront" v-lazy="form.sulegalpersonidcardfront"
+                   class="avatar">
               <i class="el-icon-plus"></i>
             </el-upload>
-            <span class="m-alert">请上传法人身份证正反面图片</span>
+            <span class="m-alert">请上传法人身份证正面图片</span>
+            <el-upload
+              :action="uploadUrl"
+              class="avatar-uploader"
+              list-type="picture-card"
+              accept="image/*"
+              :on-success="handleBackSuccess"
+              :on-preview="handlePictureCardPreview"
+              :on-remove="handleEndRemove">
+              <img v-if="form.subusinesslicense" :key="form.subusinesslicense" v-lazy="form.subusinesslicense"
+                   class="avatar">
+              <i class="el-icon-plus"></i>
+            </el-upload>
+            <span class="m-alert">请上传法人身份证反面图片</span>
           </el-form-item>
           <div class="m-text-center">
             <div>
@@ -115,36 +141,150 @@
       data(){
           return{
             form: {
+              suloginphone:'',
+              suname:'',
+              sulinkman:'',
+              sulinkphone:'',
+              suemail:'',
+              suaddress:'',
+              subanksn:'',
+              subankname:'',
+              sucontract:[],
+              subusinesslicense:'',
+              suregisteredfund:'',
+              sumaincategory:'',
+              suregisteredtime:'',
+              sulegalperson:'',
+              sulegalpersonidcardfront:'',
               name: '',
               region: '',
               checked:false
             },
             ruleForm:{
-              pcname: [
+              suloginphone: [
                 {required: true, message: '分类名必填', trigger: 'blur'}
               ],
+              suname:[
+                {required: true, message: '分类名必填', trigger: 'blur'}
+              ],
+              sulinkman:[
+                {required: true, message: '分类名必填', trigger: 'blur'}
+              ],
+              sulinkphone:[
+                {required: true, message: '分类名必填', trigger: 'blur'}
+              ],
+              suemail:[
+
+              ],
+              suaddress:[
+                {required: true, message: '分类名必填', trigger: 'blur'}
+              ],
+              subanksn:[
+
+              ],
+              subankname:[
+
+              ],
+              sucontract:[
+
+              ],
+              subusinesslicense:[
+
+              ],
+              suregisteredfund:[
+
+              ],
+              sumaincategory:[
+
+              ],
+              suregisteredtime:[
+
+              ],
+              sulegalperson:[
+
+              ],
+              sulegalpersonidcardfront:[
+
+              ]
             },
             secondStep:false,
             dialogImageUrl: '',
             dialogVisible: false,
-            userKnowe:false
+            userKnowe:false,
+            suContractUrl:[]
           }
       },
       computed: {
         uploadUrl() {
           return this.$api.upload_file
         },
+        uploadVoucherUrl() {
+          return this.$api.upload_file  + '&type=voucher'
+        },
+      },
+      watch: {
+        suContractUrl(val) {
+          this.form.sucontract = val.map(item => item.url);
+        },
       },
       methods:{
-        handleRemove(file, fileList) {
-          console.log(file, fileList);
+        //  删除合同
+        handleConstractRemove(file, fileList){
+          this.suContractUrl = fileList
         },
+        //删除营业执照
+        handleLicenseRemove(file, fileList){
+          this.form.subusinesslicense = '';
+        },
+        //删除正面
+        handleFrontRemove(){
+          this.form.sulegalpersonidcardfront = '';
+        },
+        //删除反面
+        handleEndRemove(){
+          this.form.sulegalpersonidcardback = '';
+        },
+        //预览
         handlePictureCardPreview(file) {
           this.dialogImageUrl = file.url;
           this.dialogVisible = true;
         },
+        //合同
+        uploadContract(file) {
+          let formData = new FormData();
+
+          formData.append('file', file.file)
+
+          this.$http({
+            method: 'post',
+            url: this.uploadVoucherUrl,
+            data: formData,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+          }).then(
+            res => {
+              if (res.data.status == 200) {
+                let resData = res.data,
+                  data = resData.data;
+
+                this.suContractUrl.push({
+                  name: file.file.name,
+                  url: data,
+                })
+              }
+            }
+          );
+        },
+        //营业执照
         handleHeaderSuccess(file){
-          console.log(file)
+          this.form.subusinesslicense = file.data;
+        },
+        //正面
+        handleFrontSuccess(file){
+          this.form.sulegalpersonidcardfront = file.data;
+        },
+        //反面
+        handleBackSuccess(file){
+          this.form.sulegalpersonidcardback = file.data;
         },
         beforePicUpload(file){
 
